0b62f7ca0272d3f50cd11e47974f81b55531131a
marcelwiemers <wiemers.marcel@territory.group>
2022-09-30 16:34:09 +0200

    fix update

--- pages/personal.tsx
+++ pages/personal.tsx
@@ -5,7 +5,7 @@ import {
   getDocs,
   updateDoc,
 } from 'firebase/firestore';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import AddWorkerModal from '../components/AddWorkerModal';
 import Button from '../components/Button';
@@ -25,48 +25,63 @@ const Wrapper = styled.div`
 `;
 
 const Personal = (): JSX.Element => {
-  const [workers, setWorkers] = useState<Worker[]>();
+  const [workers, setWorkers] = useState<Worker[]>([]);
   const [addModal, setAddModal] = useState<boolean>(false);
   const [modal, setModal] = useState<boolean>(false);
+  const [newWorkers, setNewWorkers] = useState<string[]>([]);
 
   useEffect(() => {
-    const readData = async () => {
-      const workRef = await collection(db, 'workers');
-      await getDocs(workRef)
-        .then((snapshot) => {
-          const array: any = [];
-          snapshot.docs.forEach((doc) => {
-            array.push({ ...doc.data(), id: doc.id });
-          });
-          setWorkers(array);
-          return array;
-        })
-        .catch((err) => console.log(err));
-    };
-    readData();
+    // const readData = async () => {
+    //   const workRef = await collection(db, 'workers');
+    //   await getDocs(workRef)
+    //     .then((snapshot) => {
+    //       const array: any = [];
+    //       snapshot.docs.forEach((doc) => {
+    //         array.push({ ...doc.data(), id: doc.id });
+    //       });
+    //       setWorkers(array);
+    //       return array;
+    //     })
+    //     .catch((err) => console.log(err));
+    // };
+    // readData();
+    setWorkers([
+      { name: 'Marcel', station: 'Licht', id: '12345' },
+      { name: 'Julian', station: 'CVD 1', id: '54321' },
+    ]);
   }, []);
 
   const handleDelete = (id: string) => {
-    console.log('Ja, na dran...');
     const workersRef = doc(db, 'workers', id);
     deleteDoc(workersRef).then(() => setModal(true));
   };
 
-  const handleUpdateStation = (event: any, id: string) => {
+  const handleUpdateStation = (event: any, index: number, id: string) => {
     const newStation = event.target.value;
-    const workersRef = doc(db, 'workers', id);
-    updateDoc(workersRef, { station: newStation }).then(() =>
-      console.log('yes')
-    );
+    const workerClone = [...workers];
+    workerClone[index].station = newStation;
+
+    setWorkers(workerClone);
+    if (!newWorkers.includes(workerClone[index].id)) {
+      setNewWorkers((old) => [...old, workerClone[index].id]);
+    }
   };
 
-  const mappedWorkers = workers?.map((el) => (
+  const handleUpdateFirebase = () => {
+    newWorkers.forEach((worker) => {
+      const newWorker = workers.find((el) => el.id == worker);
+      const workersRef = doc(db, 'workers', worker);
+      updateDoc(workersRef, { newWorker }).then(() => console.log('yes'));
+    });
+  };
+
+  const mappedWorkers = workers?.map((el, index) => (
     <Worker
       key={el.id}
       id={el.id}
       name={el.name}
       station={el.station}
-      onStationChange={(event) => handleUpdateStation(event, el.id)}
+      onStationChange={(event) => handleUpdateStation(event, index, el.id)}
       onClick={() => handleDelete(el.id)}
     />
   ));
@@ -82,6 +97,7 @@ const Personal = (): JSX.Element => {
         label="Neuen Mitarbeiter hinzufügen"
         onClick={() => setAddModal(true)}
       />
+      <Button label="Updates speichern" onClick={handleUpdateFirebase} />
     </Wrapper>
   );
 };
